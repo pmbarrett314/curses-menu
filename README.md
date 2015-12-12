896f32e2aaf627733c42595ca9f7632316dc8654
Paul Barrett <pmbarrett314@gmail.com>
2015-12-12 14:55:01 -0600

    moved acknowledgements

---
+++ README.md
@@ -0,0 +1,8 @@
+### Acknowledgements
+
+Topmenu and the submenus are based of the example found at this location http://blog.skeltonnetworks.com/2010/03/python-curses-custom-menuData/
+The rest of the work was done by Matthew Bennett and he requests you keep these two mentions when you reuse the code :-)
+Basic code refactoring by Andrew Scheller
+Menu originally from  https://gist.github.com/abishur/2482046
+
+See Also https://gist.github.com/etkirsch/53505478f53aeeac24a5
--- cursesmenu/curse_menu.py
+++ cursesmenu/curse_menu.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Topmenu and the submenus are based of the example found at this location http://blog.skeltonnetworks.com/2010/03/python-curses-custom-menuData/
-# The rest of the work was done by Matthew Bennett and he requests you keep these two mentions when you reuse the code :-)
-# Basic code refactoring by Andrew Scheller
 
-
-# This file from: https://gist.github.com/abishur/2482046
 import curses
 import os  # curses is the interface for capturing key presses on the menuData, os launches the files
 import platform

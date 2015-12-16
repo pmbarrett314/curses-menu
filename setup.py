37bcd8b356fcce2003f50770ab9df7d82ae7cf62
Paul Barrett <pmbarrett314@gmail.com>
2015-12-16 17:58:29 -0600

    used tox in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
 from setuptools import setup, find_packages
+from setuptools.command import test as TestCommand
+import sys
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
 
 
 setup(
@@ -16,9 +41,10 @@ setup(
     ],
 
     packages=find_packages(),
-    #package_dir={'': 'cursesmenu'},
+    # package_dir={'': 'cursesmenu'},
 
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
 
 )

cadab7670041cde18b2bab687cecbc4f11d5a806
Paul Barrett <pmbarrett314@gmail.com>
2015-12-17 14:50:57 -0600

    cross-platform testing

--- test/test_command_item.py
+++ test/test_command_item.py
@@ -1,12 +1,16 @@
 import unittest
 from cursesmenu.command_item import CommandItem
+import platform
 
 
 class TestCommandItem(unittest.TestCase):
     def setUp(self):
         CommandItem.set_up_terminal = lambda _: None
         CommandItem.clean_up_terminal = lambda _: None
-        self.item1 = CommandItem("Bad Command", "del", None)
+        if platform.system().lower() == "windows":
+            self.item1 = CommandItem("Bad Command", "del", None)
+        else:
+            self.item1 = CommandItem("Bad Command", "return 1", None)
 
     def test_run(self):
         self.assertEqual(self.item1.action(), 1)

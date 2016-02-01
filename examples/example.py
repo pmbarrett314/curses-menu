23a7b204ee05165310379ba15c4e1f7244f65a41
Paul Barrett <pmbarrett314@gmail.com>
2016-02-01 01:47:46 -0600

    Updates to example

--- examples/example.py
+++ examples/example.py
@@ -1,15 +1,27 @@
 from cursesmenu import *
 from cursesmenu.items import *
 
-menu = CursesMenu("Title", "Subtitle")
-item1 = MenuItem("Item", menu)
-function_item = FunctionItem("Fun item", menu, input, ["Enter an input"])
-command_item = CommandItem("Command", menu, "echo hello")
-submenu = SelectionMenu(["item1", "item2", "item3"])
-submenu_item = SubmenuItem("Submenu item", menu, submenu)
-menu.append_item(item1)
-menu.append_item(function_item)
-menu.append_item(command_item)
-menu.append_item(submenu_item)
-menu.start()
-menu.join()
+def main():
+    menu = CursesMenu("Root Menu", "Root Menu Subtitle")
+    item1 = MenuItem("Item 1", menu)
+    function_item = FunctionItem("Fun item", menu, input, ["Enter an input"])
+    command_item = CommandItem("Command", menu, "echo hello")
+    submenu = SelectionMenu(["item1", "item2", "item3"])
+    submenu_item = SubmenuItem("Submenu item", menu=menu, submenu=submenu)
+    submenu_2 = CursesMenu("Submenu Title", "Submenu subtitle")
+    function_item_2 = FunctionItem("Fun item", submenu_2, input, ["Enter an input"])
+    item2 = MenuItem("Another Item", submenu_2)
+    submenu_2.append_item(function_item_2)
+    submenu_2.append_item(item2)
+    submenu_item_2 = SubmenuItem("Another submenu", submenu=submenu_2, menu=menu)
+    menu.append_item(item1)
+    menu.append_item(function_item)
+    menu.append_item(command_item)
+    menu.append_item(submenu_item)
+    menu.append_item(submenu_item_2)
+    menu.start()
+    menu.join()
+
+
+if __name__ == "__main__":
+    main()

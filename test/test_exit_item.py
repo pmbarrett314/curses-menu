63098708d5044495a8375ed98fdae8c06153bd63
Paul Barrett <pmbarrett314@gmail.com>
2021-06-03 18:12:31 -0500

    update ExitItem tests

--- test/test_exit_item.py
+++ test/test_exit_item.py
@@ -1,21 +1,27 @@
-from base_test_case import BaseTestCase
+import pytest
 
 from cursesmenu import CursesMenu
-from cursesmenu.items import ExitItem
+from cursesmenu.items import ExitItem, SubmenuItem
 
 
-class TestExitItem(BaseTestCase):
-    def test_init(self):
-        menu = CursesMenu("menu", "test_init")
-        exit_item_1 = ExitItem("exit_item_1")
-        exit_item_2 = ExitItem("exit_item_2", menu)
-        exit_item_3 = ExitItem(text="exit_item_3", menu=menu)
-        self.assertEqual(exit_item_1.text, "exit_item_1")
-        self.assertEqual(exit_item_2.text, "exit_item_2")
-        self.assertEqual(exit_item_3.text, "exit_item_3")
-        self.assertEqual(exit_item_1.menu, None)
-        self.assertEqual(exit_item_2.menu, menu)
-        self.assertEqual(exit_item_3.menu, menu)
-        self.assertTrue(exit_item_1.should_exit)
-        self.assertTrue(exit_item_2.should_exit)
-        self.assertTrue(exit_item_3.should_exit)
+@pytest.fixture
+def exit_item():
+    return ExitItem()
+
+
+@pytest.fixture
+def exit_item_with_parent():
+    root_menu = CursesMenu("root_menu")
+    submenu = CursesMenu("submenu")
+
+    submenu_item = SubmenuItem("submenu_item_1", submenu, menu=root_menu)
+    root_menu.append_item(submenu_item)
+    return submenu.exit_item
+
+
+def test_show(exit_item):
+    assert exit_item.show("q") == "q - Exit"
+
+
+def test_show_with_parent(exit_item_with_parent):
+    assert exit_item_with_parent.show("q") == "q - Return to root_menu menu"

5969509b09f9283ba8db42ec7d50941ec39aecb3
Paul Barrett <pmbarrett314@gmail.com>
2022-07-30 21:41:28 -0500

    Testing platform specific coverage

--- test/test_command_item.py
+++ test/test_command_item.py
@@ -16,30 +16,36 @@ test_file_path = pathlib.Path("test.txt")
 
 @pytest.fixture
 def create_item():
-    if sys.platform.startswith("win"):  # pragma: no cover
+    if sys.platform.startswith(
+        "win",
+    ):  # pragma: no cover macos # pragma: no cover linux
         return CommandItem(
             "create_item",
             "echo",
             arguments=["hello"],
             stdout_filepath=test_file_path,
         )
-    else:
+    else:  # pragma: no cover windows
         return CommandItem("create_item", "echo hello", stdout_filepath=test_file_path)
 
 
 @pytest.fixture
 def delete_item():
-    if sys.platform.startswith("win"):  # pragma: no cover
+    if sys.platform.startswith(
+        "win",
+    ):  # pragma: no cover macos # pragma: no cover linux
         return CommandItem("delete_item", "del", arguments=[f"{str(test_file_path)}"])
-    else:
+    else:  # pragma: no cover windows
         return CommandItem("delete_item", f"rm {str(test_file_path)}", ["-f"])
 
 
 @pytest.fixture
 def exit_item():
-    if sys.platform.startswith("win"):  # pragma: no cover
+    if sys.platform.startswith(
+        "win",
+    ):  # pragma: no cover macos # pragma: no cover linux
         return CommandItem("return_command_item", "exit", arguments=["42"])
-    else:
+    else:  # pragma: no cover windows
         return CommandItem("return_command_item", "exit 42")
 
 

744e557fb3009ebd716930459854685643dece88
Paul Barrett <pmbarrett314@gmail.com>
2023-10-25 01:47:13 -0500

    Update lots of dev dependencies. Drop some things that ruff does.
    Fix github workflow
    
    Additional Github Workflow Fix

--- test/example_menus/menu_with_lots_of_items.py
+++ test/example_menus/menu_with_lots_of_items.py
@@ -5,7 +5,7 @@ from cursesmenu.items import MenuItem
 def main():
     menu = cursesmenu.CursesMenu("Test Menu", "subtitle")
     for i in range(100):
-        menu.items.append(MenuItem(f"item{i}", should_exit=True))  # noqa: PERF401
+        menu.items.append(MenuItem(f"item{i}", should_exit=True))
     menu.show()
 
 
